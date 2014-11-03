b38f73e4b8925a19db16fab6f114a72f23fa8968
Samuli Suomi <smsuom@utu.fi>
2014-11-03 23:06:56 +0200

    Refactor feed src and add action bar button

--- src/org/jouluristeily/risteilyohjelma14/content/FeedFragment.java
+++ src/org/jouluristeily/risteilyohjelma14/content/FeedFragment.java
@@ -2,14 +2,11 @@ package org.jouluristeily.risteilyohjelma14.content;
 
 import org.jouluristeily.risteilyohjelma14.R;
 import org.jouluristeily.risteilyohjelma14.StartActivity;
-import org.jouluristeily.risteilyohjelma14.helpers.TouchImageView;
 
 import android.annotation.SuppressLint;
 import android.content.ContentResolver;
-import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
@@ -19,22 +16,20 @@ import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 @SuppressLint("ValidFragment")
 public class FeedFragment extends SherlockFragment {
 
     private int mPos = -1;
-    private static ImageView karttanappi_ostokset;
-    private static ImageView karttanappi_baarit;
-    private static ImageView karttanappi_ravintolat;
-    private static TouchImageView kartta;
-    private static boolean[] toggleStates;
+    private static WebView feedView;
     private static final String URL_FEED = "http://sasuomi.github.com/risteilyfeed";
 
     public FeedFragment() {
@@ -53,35 +48,21 @@ public class FeedFragment extends SherlockFragment {
         }
         final LinearLayout feedlayout = (LinearLayout) inflater.inflate(
                 R.layout.fragment_feed, container, false);
-        WebView feedView = (WebView) feedlayout.findViewById(R.id.feedView);
-        feedView.setWebViewClient(new WebViewClient());
-        feedView.setBackgroundColor(getResources().getColor(R.color.fragment_tausta));
-        feedView.getSettings().setAppCachePath(getActivity().getCacheDir().getAbsolutePath());
-        feedView.getSettings().setAllowFileAccess(true);
-        feedView.getSettings().setAppCacheEnabled(true);
-        feedView.getSettings().setJavaScriptEnabled(true);
-        feedView.getSettings().setCacheMode( WebSettings.LOAD_DEFAULT ); // load online by default
-        // Roaming:
-        ContentResolver cr = getActivity().getContentResolver();
-        int isRoaming = 1;
-        try {
-			isRoaming = Settings.Secure.getInt(cr, Settings.Secure.DATA_ROAMING);
-		} catch (SettingNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-        if (!isNetworkAvailable() || isRoaming == 1) { // loading offline
-            CharSequence text = "Ei verkkoyhteyttä tai verkkovierailu käynnissä";
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(
-                    getSherlockActivity(), text, duration);
-            toast.show();
-        	feedView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ONLY);
-        }
+        feedView = (WebView) feedlayout.findViewById(R.id.feedView);
         WebSettings webSettings = feedView.getSettings();
+        feedView.setWebViewClient(new WebViewClient());
+        feedView.setBackgroundColor(getResources().getColor(
+                R.color.fragment_tausta));
+        webSettings.setAppCachePath(getActivity().getCacheDir()
+                .getAbsolutePath());
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAppCacheEnabled(true);
         webSettings.setJavaScriptEnabled(true);
-        feedView.loadUrl(URL_FEED);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        refreshFeed();
+
         setHasOptionsMenu(true);
         setRetainInstance(true);
         getSherlockActivity().getSupportActionBar().setLogo(
@@ -97,37 +78,67 @@ public class FeedFragment extends SherlockFragment {
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
     }
-    
-    private boolean isNetworkAvailable() {
-    	boolean isConnected = false;
-    	ConnectivityManager check = (ConnectivityManager)        
-    	getActivity().getSystemService(getActivity().CONNECTIVITY_SERVICE);
-    	if (check != null){ 
-    		NetworkInfo[] info = check.getAllNetworkInfo();
-    		if (info != null){
-    			for (int i = 0; i <info.length; i++){
-    				if (info[i].getState() == NetworkInfo.State.CONNECTED){
-    					isConnected = true;
-    				}
-    			}
-    		}
-    	}
-    	return isConnected;
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.action_bar_feed, menu);
     }
-   
-    /*
-    private class MyWebViewClient extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (Uri.parse(url).getHost().equals(URL_FEED)) {
-                // This is my web site, so do not override; let my WebView load the page
-                return false;
-            }
-            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            startActivity(intent);
-            return true;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.btn_paivita_feed) {
+            refreshFeed();
         }
+        return super.onOptionsItemSelected(item);
     }
-    */
+
+    private void refreshFeed() {
+        // Roaming:
+        ContentResolver cr = getActivity().getContentResolver();
+        int isRoaming = 1;
+        try {
+            isRoaming = Settings.Secure
+                    .getInt(cr, Settings.Secure.DATA_ROAMING);
+        } catch (SettingNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        if (!isNetworkAvailable() || isRoaming == 1) { // loading offline
+            CharSequence text = "Ei verkkoyhteyttä tai verkkovierailu käynnissä";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(getSherlockActivity(), text, duration);
+            toast.show();
+            feedView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ONLY);
+        }
+        feedView.loadUrl(URL_FEED);
+    }
+
+    private boolean isNetworkAvailable() {
+        boolean isConnected = false;
+        ConnectivityManager check = (ConnectivityManager) getActivity()
+                .getSystemService(getActivity().CONNECTIVITY_SERVICE);
+        if (check != null) {
+            NetworkInfo[] info = check.getAllNetworkInfo();
+            if (info != null) {
+                for (int i = 0; i < info.length; i++) {
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        isConnected = true;
+                    }
+                }
+            }
+        }
+        return isConnected;
+    }
+
+    /*
+     * private class MyWebViewClient extends WebViewClient {
+     * 
+     * @Override public boolean shouldOverrideUrlLoading(WebView view, String
+     * url) { if (Uri.parse(url).getHost().equals(URL_FEED)) { // This is my web
+     * site, so do not override; let my WebView load the page return false; } //
+     * Otherwise, the link is not for a page on my site, so launch another
+     * Activity that handles URLs Intent intent = new Intent(Intent.ACTION_VIEW,
+     * Uri.parse(url)); startActivity(intent); return true; } }
+     */
 }
